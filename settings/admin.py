8273aae24d79bc3f82ba38f695bb8366237b9e0a
python-brain-developer <badi.al.zaman2446@gmail.com>
2023-07-26 15:14:15 -0800

    add settings models

--- settings/admin.py
+++ settings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models 
 
 # Register your models here.
+admin.site.register(models.About)
+admin.site.register(models.Faq)
+admin.site.register(models.Info)
